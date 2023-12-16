451c983b2ce83a62a06de849e6085c00c0e63ef7
Yuanbin Shen <yuanbin.shen@gmail.com>
2023-12-15 18:19:05 -0800

    Put numword implementation in separate file

--- keyboards/moonlander/keymaps/hrm/keymap.c
+++ keyboards/moonlander/keymaps/hrm/keymap.c
@@ -1,6 +1,7 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
 #include "swapper.h"
+#include "num_word.h"
 
 #define MOON_LED_LEVEL LED_LEVEL
 #define TR KC_TRANSPARENT
@@ -88,106 +89,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 
-/* ==== start: numword ==== */
-static uint16_t num_word_timer = 0;
-static bool is_num_word_on = false;
-
-bool is_num_word_enabled(void) {
-    return is_num_word_on;
-}
-
-void enable_num_word(void) {
-    if (is_num_word_on) return;
-    is_num_word_on = true;
-    layer_on(L_NUMBERS);
-}
-
-void disable_num_word(void) {
-    if (!is_num_word_on) return;
-    is_num_word_on = false;
-    layer_off(L_NUMBERS);
-}
-
-void toggle_num_word(void) {
-    if (is_num_word_on) {
-        disable_num_word();
-    }
-    else {
-        enable_num_word();
-    }
-}
-
-bool should_terminate_num_word(uint16_t keycode, const keyrecord_t *record) {
-    switch (keycode) {
-        // Keycodes which should not disable num word mode.
-        // We could probably be more brief with these definitions by using
-        // a couple more ranges, but I believe "explicit is better than
-        // implicit"
-    case KC_1 ... KC_0:
-    case KC_DOT:
-        return false;
-    default:
-        if (record->event.pressed) {
-            return true;
-        }
-        return false;
-    }
-    // Should be unreachable
-    return false;
-}
-
-bool process_record_num_word(uint16_t keycode, const keyrecord_t *record) {
-    // Handle the custom keycodes that go with this feature
-    if (keycode == NUMWORD) {
-        if (record->event.pressed) {
-            enable_num_word();
-            num_word_timer = timer_read();
-            return false;
-        }
-        else {
-            if (timer_elapsed(num_word_timer) > TAPPING_TERM) {
-                // If the user held the key longer than TAPPING_TERM,
-                // consider it a hold, and disable the behavior on
-                // key release.
-                disable_num_word();
-                return false;
-            }
-        }
-    }
-
-    // Other than the custom keycodes, nothing else in this feature will
-    // activate if the behavior is not on, so allow QMK to handle the
-    // event as usual
-    if (!is_num_word_on) return true;
-
-    // Nothing else acts on key release, either
-    if (!record->event.pressed) {
-        return true;
-    }
-
-    // Get the base keycode of a mod or layer tap key
-    switch (keycode) {
-    case QK_MOD_TAP ... QK_MOD_TAP_MAX:
-    case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
-    case QK_TAP_DANCE ... QK_TAP_DANCE_MAX:
-        // Earlier return if this has not been considered tapped yet
-        if (record->tap.count == 0)
-            return true;
-        keycode = keycode & 0xFF;
-        break;
-    default:
-        break;
-    }
-
-    if (should_terminate_num_word(keycode, record)) {
-        disable_num_word();
-    }
-
-    return true;
-}
-
-/* ==== end: numword ==== */
-
 bool sw_win_active = false;
 bool sw_tab_active = false;
 
@@ -195,7 +96,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!update_swapper(&sw_win_active, KC_LALT, KC_TAB, SW_WIN, keycode, record))
         return false; // has update, stop process
 
-    if (!process_record_num_word(keycode, record))
+    if (!process_record_num_word(NUMWORD, L_NUMBERS, keycode, record))
         return false;
 
     switch (keycode) {
--- keyboards/moonlander/keymaps/hrm/rules.mk
+++ keyboards/moonlander/keymaps/hrm/rules.mk
@@ -9,3 +9,4 @@ SPACE_CADET_ENABLE = no
 CAPS_WORD_ENABLE = yes
 
 SRC += ./swapper.c
+SRC += ./num_word.c
