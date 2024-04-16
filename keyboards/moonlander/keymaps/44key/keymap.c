031e45d7ab2e78360d9901752a5fac89f4ca504b
Yuanbin Shen <ybshen@fortinet.com>
2024-04-16 10:40:46 -0700

    Adjust: swap pgup/pgdn with home/end; fn; extra dot on num layer; mouse keys on base layer

--- keyboards/moonlander/keymaps/44key/keymap.c
+++ keyboards/moonlander/keymaps/44key/keymap.c
@@ -53,9 +53,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [L_BASE] = LAYOUT_moonlander(
 	_______, _______, _______, _______, _______, _______, _______,
 	_______, _______, _______, _______, _______, _______, _______,
-	KC_ESCAPE, KC_Q,     LT(3,KC_W),     LT(2,KC_E),     LT(5,KC_R), MT(MOD_LGUI,KC_T),  _______,
+	KC_ESCAPE, KC_Q,     LT(3,KC_W),     LT(2,KC_E),     LT(5,KC_R), MT(MOD_LGUI,KC_T),  KC_MS_BTN2,
 	_______, MT(MOD_RGUI,KC_Y),     LT(5,KC_U),     LT(2,KC_I),     LT(3,KC_O), KC_P,  _______,
-	SW_WIN,MT(MOD_LSFT, KC_A),MT(MOD_LALT, KC_S),MT(MOD_LCTL, KC_D),LT(1,KC_F),     LT(4,KC_G),     _______,
+	SW_WIN,MT(MOD_LSFT, KC_A),MT(MOD_LALT, KC_S),MT(MOD_LCTL, KC_D),LT(1,KC_F),     LT(4,KC_G),     KC_MS_BTN1,
 	_______, LT(4,KC_H),     LT(1,KC_J),     MT(MOD_RCTL, KC_K),MT(MOD_RALT, KC_L),MT(MOD_RSFT, KC_SCLN),ST_MACRO_RAR_N,
 	NUMWORD, KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,
 	KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       CW_TOGG,
@@ -80,27 +80,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [L_NUMNAV] = LAYOUT_moonlander(
 	_______, _______, _______, _______, _______, _______, _______,
 	_______, _______, _______, _______, _______, _______, _______,
-	_______, KC_WWW_FORWARD, KC_PGUP,        KC_UP,        KC_PGDN,       ST_MACRO_ZOOMOUT, _______,
+	_______, KC_WWW_FORWARD, KC_HOME,        KC_UP,        KC_END,       ST_MACRO_ZOOMOUT, _______,
 	_______, _______, KC_7,           KC_8,           KC_9,           KC_ASTR,        _______,
 	_______, KC_WWW_BACK, KC_LEFT,    KC_DOWN,         KC_RIGHT, ST_MACRO_ZOOMIN,        _______,
 	_______, _______, KC_4,           KC_5,           KC_6,           KC_DOT,        KC_MINUS,
-	_______, KC_WWW_HOME, KC_HOME, _______,    KC_END, ST_MACRO_ZOOM_RESET,
+	_______, KC_WWW_HOME, KC_PGUP, _______,    KC_PGDN, ST_MACRO_ZOOM_RESET,
 	_______, KC_1,           KC_2,           KC_3,           KC_SLASH,       _______,
 	QK_BOOT,        _______, KC_PSCR,        KC_DELETE,      KC_APP,  _______,
 	_______, KC_0,           KC_PLUS,         KC_EQUAL,       _______, KC_CAPS,
-	_______, _______, _______,                 _______, _______, ST_MACRO_RAR
+	KC_DOT, _______, _______,                 _______, _______, ST_MACRO_RAR
 	),
     [L_FNMOUSE] = LAYOUT_moonlander(
 	_______, _______, _______, _______, _______, _______, _______,
 	_______, _______, _______, _______, _______, _______, _______,
 	_______, _______, KC_MS_WH_UP,  KC_MS_UP,       KC_MS_WH_DOWN, _______, _______,
-	_______, _______, KC_F7,          KC_F8,          KC_F9,          _______, _______,
+	_______, KC_F12, KC_F7,          KC_F8,          KC_F9,          KC_F15, _______,
 	_______, KC_MS_WH_LEFT,    KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    KC_MS_WH_RIGHT,  _______,
-	_______, _______, KC_F4,          KC_F5,          KC_F6,          _______, _______,
+	_______, KC_F11, KC_F4,          KC_F5,          KC_F6,          KC_F14, _______,
 	_______, _______, _______, _______, _______, _______,
-	_______, KC_F1,          KC_F2,          KC_F3,          _______, _______,
+	KC_F10, KC_F1,          KC_F2,          KC_F3,          KC_F13, _______,
 	_______, _______, _______, _______, KC_MS_BTN3,     _______,
-	_______, KC_F10,         KC_F11,         KC_F12,         _______, _______,
+	_______, _______,         _______,         _______,         _______, _______,
 	KC_MS_BTN1, KC_MS_BTN2, _______,                 _______, _______, _______
 	),
     [L_MACSYS] = LAYOUT_moonlander(
